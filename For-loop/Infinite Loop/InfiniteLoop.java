62cfaafd9e7257fae05d79c44d42349d192e997f
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-06-28 19:46:33 +0545

    java codes uploaded

---
+++ For-loop/Infinite Loop/InfiniteLoop.java
@@ -0,0 +1,11 @@
+public class InfiniteLoop{
+    public static void main(String[] args){
+        for (;;) {
+            System.out.println("This is an infinite loop.");
+            // To stop the loop, you can use Ctrl+C in the console.
+            // Or you can add a break condition to exit the loop.
+            // Uncomment the next line to break after one iteration.
+            // break;
+        }
+    }
+}
