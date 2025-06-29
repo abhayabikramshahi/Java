444bcbe8b305c2b8e89dd3cddb2fcb03bf814655
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-06-29 11:21:36 +0545

    upload moreee assests

---
+++ Multiple Variable/Variable.java
@@ -0,0 +1,9 @@
+public class Variable{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        int z = 12;
+        System.out.println(x + y + z);
+
+    }
+}
---
+++ Variable/Variable.java
@@ -0,0 +1,8 @@
+public class Variable{
+    public static void main(String[] args){
+        String name = "Abhaya";
+        int age = 15;
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+    }
+}
