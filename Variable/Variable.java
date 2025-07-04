86eaab2b31bcb24ebc0d9b9f43aec97844e9de8a
Abhaya Bikram Shahi <abhayabikramshahiofficial@gmail.com>
2025-07-04 20:22:03 +0545

    Delete Variable directory

--- Variable/Variable.java
+++
@@ -1,8 +0,0 @@
-public class Variable{
-    public static void main(String[] args){
-        String name = "Abhaya";
-        int age = 15;
-        System.out.println("Name:" + name);
-        System.out.println("Age:" + age);
-    }
-}
